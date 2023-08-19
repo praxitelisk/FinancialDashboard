17ee3a1105b6c2de8f283276cc6667df29048ae7
Praxitelis-Nikolaos Kouroupetroglou <praxitelisk@gmail.com>
2023-08-19 12:53:08 +0300

    Testing new StreamLit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,36 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import numpy as np
 
-# Title and description
-st.title("Iris Dataset Analysis")
-st.write("This app performs descriptive analysis and visualization on the Iris dataset.")
+st.title('Uber pickups in NYC')
 
-# Load the Iris dataset
-@st.cache  # Use caching to load the data only once
-def load_data():
-    data = sns.load_dataset("iris")
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-data = load_data()
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
 
-# Display raw data
-st.subheader("Raw Data")
-st.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
-# Descriptive statistics
-st.subheader("Descriptive Statistics")
-st.write(data.describe())
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
-# Sidebar options
-selected_feature = st.sidebar.selectbox("Select a feature", data.columns[:-1])
-selected_species = st.sidebar.multiselect("Select species", data["species"].unique())
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-# Filtering based on sidebar inputs
-filtered_data = data[data["species"].isin(selected_species)]
-
-# Display scatter plot
-st.subheader("Scatter Plot")
-plt.figure(figsize=(8, 6))
-sns.scatterplot(x=selected_feature, y="sepal_width", hue="species", data=filtered_data)
-plt.xlabel(selected_feature)
-plt.ylabel("Sepal Width")
-plt.title(f"{selected_feature} vs Sepal Width")
-st.pyplot()
-
-# Display box plot
-st.subheader("Box Plot")
-plt.figure(figsize=(8, 6))
-sns.boxplot(x="species", y=selected_feature, data=filtered_data)
-plt.xlabel("Species")
-plt.ylabel(selected_feature)
-plt.title(f"{selected_feature} by Species")
-st.pyplot()
-
-# Display pair plot
-st.subheader("Pair Plot")
-sns.pairplot(data=filtered_data, hue="species")
-st.pyplot()
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
