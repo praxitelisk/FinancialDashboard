9ae68bbc646adc070d06b5be3a7bc4ea6e03e319
Praxitelis-Nikolaos Kouroupetroglou <praxitelisk@gmail.com>
2023-10-14 19:23:01 +0300

    Changing the icon.

--- pages/b. Financial Indicators.py
+++ pages/b. Financial Indicators.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Financial Indicators", page_icon="📉")
+st.set_page_config(page_title="Financial Indicators", page_icon="🧮")
 
 st.markdown("# Stock Closing Price and Financial Indicators")
 st.sidebar.header("Financial Indicators")
